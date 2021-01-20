c90c398280da3903baeb917ae08db70b58f6aa58
teemuhamberg <58030176+teemuhamberg@users.noreply.github.com>
2021-01-20 11:09:57 +0200

    Add files via upload

--- DHT.ino
+++ DHT.ino
@@ -1,4 +1,3 @@
-
 #include "DHT.h"
 #include <LiquidCrystal.h>
 
@@ -10,8 +9,8 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; // LCD pinnien mää
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 DHT dht(DHTPIN, DHTTYPE);
-int maxHum = 50;  //määritetään kosteus milloin tuuletin menee päälle
-int maxTemp = 30; //määritetään lämpötila milloin tuuletin menee päälle
+int maxHum = 40;  //määritetään kosteus milloin tuuletin menee päälle
+int maxTemp = 28; //määritetään lämpötila milloin tuuletin menee päälle
 
 void setup() {
   pinMode(fan, OUTPUT);
@@ -27,7 +26,7 @@ void setup() {
 }
 
 void loop() {
-  delay(200); // viive
+  delay(1000); // viive
 
   float h = dht.readHumidity();
   // Lue lämpötila
@@ -41,23 +40,21 @@ void loop() {
     lcd.print(h);
     lcd.setCursor(12,0); 
     lcd.print(t); 
-    delay(500);
+       
     if (Serial.available()){
       lcd.write(Serial.read());
+
     }
    }
-  
-  if(h > maxHum || t > maxTemp) {
+    if(h > maxHum || t > maxTemp) {
       digitalWrite(fan, HIGH);
   } else {
      digitalWrite(fan, LOW); 
   }
-  
-  Serial.print("Humidity: "); 
+  Serial.print("Humidity "); 
   Serial.print(h);
   Serial.print(" %\t");
-  Serial.print("Temperature: "); 
+  Serial.print("Temperature "); 
   Serial.print(t);
   Serial.println(" *C ");
-
 }
